5867a376f0588ad14b6b54d21a874ba76eb85c96
Alec Sheperd <alecmshep@gmail.com>
2016-03-27 17:16:19 -0500

    You can search for recipes? That's it!

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -5,7 +5,7 @@ class RecipesController < ApplicationController
   
   def index
     @user = User.find(session[:user_id])
-    @recipes = User.recipes
+    @recipes = @user.recipes
   end
 
   def new
@@ -16,24 +16,25 @@ class RecipesController < ApplicationController
   def create
   end
   
-  def show
-  end
+ # def show
+  #end
 
   def destroy
   end
 
   def recipe_params
-    params.require(:recipe)
+    params
+#.require(:recipe).permit(:cuisine, :diet, :number, :offset, :query)
   end
 
-  def search
+  def show
     search_params = recipe_params
 
-    uri = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/search?cuisine=#{search_params[cuisine]}&diet=#{search_params[diet]}&number=#{search_params[number]}&offset=#{search_params[offset]}&query=#{search_params[query]}"
+    uri = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/search?cuisine=#{search_params[:cuisine]}&diet=#{search_params[:diet]}&number=#{search_params[:number]}&offset=#{search_params[:offset]}&query=#{search_params[:query]}"
     
-    @resp = Unirest.get uri
-    
+    @resp = Unirest.get uri, headers:{ "X-Mashape-Key" => "o0PEFR8zTDmshlk3dBI4YmNg7O2ep12IyWrjsng7WgOycYs0bF"}
+        
   end
 
-  #private :recipe_params
+  private :recipe_params
 end
