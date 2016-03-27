0b6666bd7717f39ac48b591d4d14630c4599491b
Alec Sheperd <alec.sheperd@ligo.org>
2016-03-27 18:17:07 -0500

    Had set grocery_list as grocery_lists implying a user has mutliple grocery lists. Also changed some of the URLs on the home bar

--- db/migrate/20160325004229_create_items.rb
+++ db/migrate/20160325004229_create_items.rb
@@ -2,7 +2,7 @@ class CreateItems < ActiveRecord::Migration
   def change
     create_table :items do |t|
       t.string :name
-      t.references :grocery_lists, index: true, foreign_key: true
+      t.references :grocery_list, index: true, foreign_key: true
 
       t.timestamps null: false
     end
