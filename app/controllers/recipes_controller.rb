e28d0db48d51a8289651618c3d71ec87ed63129f
Alec Sheperd <alecmshep@gmail.com>
2016-03-27 15:34:39 -0500

    recipe search being commited so I can make a new branch

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,4 +1,39 @@
+require 'unirest'
+require 'json'
+
 class RecipesController < ApplicationController
-	def recipes
-	end
+  
+  def index
+    @user = User.find(session[:user_id])
+    @recipes = User.recipes
+  end
+
+  def new
+    @user = User.find(session[:user_id])
+    @recipe = Recipe.new
+  end
+  
+  def create
+  end
+  
+  def show
+  end
+
+  def destroy
+  end
+
+  def recipe_params
+    params.require(:recipe)
+  end
+
+  def search
+    search_params = recipe_params
+
+    uri = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/search?cuisine=#{search_params[cuisine]}&diet=#{search_params[diet]}&number=#{search_params[number]}&offset=#{search_params[offset]}&query=#{search_params[query]}"
+    
+    @resp = Unirest.get uri
+    
+  end
+
+  #private :recipe_params
 end
