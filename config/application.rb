894fba52da362bc444458e3f2cdb7d4a13914862
Andrew Meyer <meyerat@uwm.edu>
2016-04-19 20:34:10 -0500

    Install minitest-rails-capybara

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,10 @@ module GroceryTracker
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # Use Minitest spec DSL for generating new tests.
+    config.generators do |g|
+      g.test_framework :minitest, spec: true
+    end
   end
 end
