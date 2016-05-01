a81c99bf7357b3bcca0e07c4ae1e9f44d31d009f
Craig Hand Luke <academichw@gmail.com>
2016-04-30 19:58:32 -0500

    fixed failure notification bug

--- app/mailers/grocery_list_mailer.rb
+++ app/mailers/grocery_list_mailer.rb
@@ -1,16 +1,16 @@
 require 'gmail'
 class GroceryListMailer < ActionMailer::Base#ApplicationMailer
-  default from: 'grocerylisttracker@gmail'
+  default from: 'grocerylisttracker@gmail.com'
    
   def low_inventory_email(arr)
     string = ""
     arr.each do |item|
-      string = string + ", " + item 		
+      string = string + " " + item 		
 	end
 	
     gmail = Gmail.new('grocerylisttracker', 'uwmcapstone')
       gmail.deliver do
-	    to "grocerylisttracker@gmail"
+	    to "grocerylisttracker@gmail.com"
 	    subject "Your GroceryListTracker Inventory is running low!"
 	    text_part do
 	      body "Need to replenish " + string
